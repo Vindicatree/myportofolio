8a96f99f91a7534da7f773035e4177acea9d6ed7
Orrick <orrickhatmoko@gmail.com>
2026-09-01 18:23:33 +0700

    HTML & CSS

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -52,12 +52,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myportofolio.urls'
+ROOT_URLCONF = 'portofolio.urls'
+WSGI_APPLICATION = 'portofolio.wsgi.application'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,6 +70,8 @@ TEMPLATES = [
     },
 ]
 
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 WSGI_APPLICATION = 'myportofolio.wsgi.application'
 
 
