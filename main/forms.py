3303f9c3b7160cd384d24877e6d8535711051777
Orrick <orrickhatmoko@gmail.com>
2026-09-19 21:03:46 +0700

    Checklists HTML & modelForm

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,60 @@
-from django.forms import ModelForm, TextInput, Textarea, URLInput
+from django.forms import ModelForm, TextInput, Textarea, URLInput, DateInput
 
-from main.models import Achievements
+from main.models import Achievements, Experience
+
+class ExperienceForm(ModelForm):
+    class Meta:
+        model = Experience
+        fields = [
+            "title",
+            "description",
+            "category",
+            "thumbnail",
+            "started_at",
+            "ended_at",
+        ]
+
+        labels = {
+            "title": "Nama Pengalaman",
+            "description": "Deskripsi pengalaman",
+            "category": "kategori pengalaman yang diperoleh",
+            "thumbnail": "URL gambar pengalaman",
+            "started_at": "Waktu mulai pengalaman",
+            "ended_at": "Waktu akhir pengalaman",
+        }
+
+        widgets = {
+            "title": TextInput(
+                attrs={
+                    "placeholder": "Judul pengalaman",
+                    "maxlength": 255,
+                }
+            ),
+            "description": Textarea(
+                attrs={
+                    "placeholder": "Ceritakan pengalamanmu",
+                    "rows": 3,
+                }
+            ),
+            "category": TextInput(
+                attrs={
+                    "placeholder": "certification, competition, award, scholarship, publication, other",
+                }
+            ),
+            "thumbnail": URLInput(
+                attrs={
+                    "placeholder": "https://drive.google.com/thumbnail?id=...&sz=w1000",
+                }
+            ),
+            "started_at": DateInput(
+                attrs={"type": "date"},
+                format="%Y-%m-%d",
+            ),
+            "ended_at": DateInput(
+                attrs={"type": "date"},
+                format="%Y-%m-%d",
+            ),
+        }
 
 class AchievementsForm(ModelForm):
     class Meta:
